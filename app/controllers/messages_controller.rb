3cbb4102207157d710cf94c990c2c9ff0e9d61db
uuukkkkk <usgwkig@gmail.com>
2015-09-19 07:31:47 +0000

    destroy function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message,only: [:edit, :update]
+  before_action :set_message,only: [:edit, :update, :destroy]
   def index
     # Messageを全て取得する
     @messages = Message.all
@@ -30,6 +30,11 @@ class MessagesController < ApplicationController
       # 保存に失敗した場合
       render 'edit'
     end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice:'メッセージを削除しました'
   end
   
   private

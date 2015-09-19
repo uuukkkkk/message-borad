b9e8512a373e85c8ae6db613d0dc1cee1304166c
uuukkkkk <usgwkig@gmail.com>
2015-09-19 09:04:49 +0000

    challenge2 push

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum:20 } , presence:true
     #内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum:2 , maximum:30 } , presence:true
+    #年齢は18歳以上60歳以内大文字小文字を区別しない
+    validates :age , inclusion: {in: 18..60} , presence:true, uniqueness: { case_sensitive: false }
 end
